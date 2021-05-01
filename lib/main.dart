3a0411a9ff32320d45496d955600e3eea487a752
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 01:37:44 +0530

    Add a Column() widget as a child of the Row() widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
                 width: 100,
                 color: Colors.red,
               ),
+              Column()
             ],
           ),
         ),
