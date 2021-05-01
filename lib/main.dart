2e1a70051b94dac95e3b1a079e4731888be5f44e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 01:29:06 +0530

    pass MyApp() inside the runApp() widget inside the main() method

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+      ),
     );
   }
 }
