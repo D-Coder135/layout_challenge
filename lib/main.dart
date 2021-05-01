bb7cd19f6fe68236b737730da1bd9b3431fdb107
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 01:27:05 +0530

    return MaterialApp() along with a Scaffold() class in its home

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
