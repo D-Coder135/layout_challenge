8229210488a75454ae023124c6c7f70f0169a225
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 01:23:56 +0530

    add a class MyApp inside main.dart file and extends the StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
