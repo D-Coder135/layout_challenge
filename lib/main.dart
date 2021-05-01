b773a6ddf98842fe591ff76c9e86018d26a0d9cb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-02 01:36:35 +0530

    set color of the Container() widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               Container(
                 width: 100,
+                color: Colors.red,
               ),
             ],
           ),
